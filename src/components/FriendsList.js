2b469bd5703fbedbe63599ffa2568dc8a5a0f333
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-02-16 12:31:51 -0600

    wip

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,5 +1,6 @@
 import React from "react";
 // 👉 1- FriendsList renders several Friend components, import Friend
+import Friend from "./Friend";
 
 export default function FriendsList(props) {
   // 👉 2- What data does FriendsList need to do its job? Use destructuring
