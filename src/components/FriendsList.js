c2df265aa5d4284bc85bec06c6f325b5e97787b1
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-02-16 12:30:18 -0600

    wip

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function FriendsList(props) {
   // 👉 2- What data does FriendsList need to do its job? Use destructuring
-  const { friends } = props;
+  const { friends, changeStatus } = props;
   return (
     <div className="list-friends container">
       {/* 👉 3- We need to loop over the data rendering a Friend as we go */}
