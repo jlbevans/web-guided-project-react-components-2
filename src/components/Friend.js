d8284972b64e7ee96fcd0e35791280559984d826
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-02-16 13:05:28 -0600

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -22,6 +22,9 @@ export default function Friend(props) {
             Likes:
             <ul>
               {/* 👉 3- Loop over the friend's hobbies and generate <li/> elements as you go */}
+              {friend.hobbies.map((hobby) => {
+                return <li key={hobby}>{hobby}</li>;
+              })}
             </ul>
           </div>
         </div>
